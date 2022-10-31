f4d1c64b381183312b23d735643e26b218f59eca
Matheus Hugolini <mathugolini@gmail.com>
2022-10-31 17:09:20 -0300

    Interface Veiculo Controller Impl Find By Id

---
+++ src/main/java/br/com/veiculo/apiveiculo/command/impl/VeiculoControllerImpl.java
@@ -0,0 +1,22 @@
+package br.com.veiculo.apiveiculo.command.impl;
+
+import br.com.veiculo.apiveiculo.command.VeiculoCommand;
+import br.com.veiculo.apiveiculo.model.Veiculo;
+import br.com.veiculo.apiveiculo.repositories.VeiculoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class VeiculoControllerImpl implements VeiculoCommand{
+
+    @Autowired
+    private VeiculoRepository repository;
+
+    @Override
+    public Veiculo findById(Integer id) {
+        Optional<Veiculo> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
